c545e8dba9f0567dfc93b77c638ae49660888a0e
aegisfleet <aegisfleet@gmail.com>
2024-08-19 21:43:06 +0900

    faviconの設定を追加

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   render() {
@@ -7,6 +7,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon-192x192.png" />
+          <link rel="icon" href="/icon-512x512.svg" type="image/svg+xml" />
           <meta name="theme-color" content="#4CAF50" />
         </Head>
         <body>
